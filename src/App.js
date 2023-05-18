fef45b465866b1bb09f93ee4fdb7ba2a1449324e
j-parthiv <parthivjani9@gmail.com>
2023-05-18 18:32:19 +0200

    Create Dummy Data and use UUID package

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import './App.css';
 import Alert from './components/Alert';
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from './components/ExpenseList';
+import { v4 as uuidv4 } from 'uuid';
+
+const initialExpenses = [
+  {id:uuidv4(), charge:"rent", amount:1600},
+  {id:uuidv4(), charge:"car payment", amount:400},
+  {id:uuidv4(), charge:"credit card bill", amount:1200}
+]
+console.log(initialExpenses)
+
 function App() {
   return (
   <div>
