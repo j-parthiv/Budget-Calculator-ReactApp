2fae049daf961bd433f7deb6df7a12115986227a
j-parthiv <parthivjani9@gmail.com>
2023-05-18 18:50:22 +0200

    Add Heading and Total Price Logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import './App.css';
-import Alert from './components/Alert';
-import ExpenseForm from './components/ExpenseForm';
-import ExpenseList from './components/ExpenseList';
-import { v4 as uuidv4 } from 'uuid';
+import { useState } from "react";
+import "./App.css";
+import Alert from "./components/Alert";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+import { v4 as uuidv4 } from "uuid";
 
 const initialExpenses = [
-  {id:uuidv4(), charge:"rent", amount:1600},
-  {id:uuidv4(), charge:"car payment", amount:400},
-  {id:uuidv4(), charge:"credit card bill", amount:1200}
-]
-console.log(initialExpenses)
+  { id: uuidv4(), charge: "rent", amount: 1600 },
+  { id: uuidv4(), charge: "car payment", amount: 400 },
+  { id: uuidv4(), charge: "credit card bill", amount: 1200 },
+];
 
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
+
   return (
-  <div>
-    <Alert/>
-    <ExpenseForm/>
-    <ExpenseList/>
-  </div>
+    <>
+      <Alert />
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList />
+      </main>
+      <h1>
+        total spending : {" "}
+        <span className="total">
+        ${" "}
+        {expenses.reduce((acc, cur)=>{
+          return acc += cur.amount;
+        },0)}
+        </span>
+      </h1>
+    </>
   );
 }
 
