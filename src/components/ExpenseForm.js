c71702e45091f011ee4791225b68e19ac5079d45
j-parthiv <parthivjani9@gmail.com>
2023-05-18 21:27:35 +0200

    Add Form to Add  New Expenses

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { MdSend } from 'react-icons/md'
 
 const ExpenseForm = () => {
   return (
-    <div>ExpenseForm</div>
+    <form>
+        <div className='form-center'>
+            <div className='form-group'>
+                <label htmlFor='charge'>Charge</label>
+                <input
+                    type='text'
+                    className='form-control'
+                    id='charge'
+                    name='charge'
+                    placeholder='e.g. rent'
+                />
+            </div>
+            <div className='form-group'>
+                <label htmlFor='label'>amount</label>
+                <input
+                    type='text'
+                    className='form-control'
+                    id='amount'
+                    name='amount'
+                    placeholder='e.g. 100'
+                />
+            </div>
+        </div>
+            <button type='submit' className='btn'>
+                submit
+            <MdSend className='btn-icon'/>
+            </button>
+    </form>
   )
 }
 
